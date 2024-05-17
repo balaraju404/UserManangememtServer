d261496500a6ecde5854b538e6c41246ff890634
balaraju404 <balarajugandham1@gmail.com>
2024-05-17 11:38:17 +0530

    commit 2

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ process.on('SIGINT', () => {
 });
 
 // Server start
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
